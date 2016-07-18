0ae4b7691d60070b64b70111e5105b9b9bda0834
Pillys <pillys@163.com>
2016-07-19 00:25:05 +0800

    ++

--- js/my-app.js
+++ js/my-app.js
@@ -2,14 +2,14 @@ var myApp = new Framework7({
   pushState: true
 });
 var $$ = Dom7;
+var ua = window.navigator.userAgent.toLowerCase();
+var isWeixin = /MicroMessenger/i.test(navigator.userAgent);
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
 myApp.onPageInit('about', function (page) {
-  $$('.create-page').on('click', function () {
-    createContentPage();
-  });
+  mainView.hideNavbar();
 });
 
 myApp.onPageInit('index', function (page) {
@@ -24,6 +24,11 @@ myApp.onPageInit('index', function (page) {
     e.preventDefault();
   });
 });
+
+if(isWeixin) {
+  mainView.hideNavbar();
+  $$('.navbar-through').removeClass('navbar-through').addClass('no-navbar');
+}
 
 if (location.pathname === '/') {
   mainView.router.loadPage('/');
