0a9530fe95c60ebcdd53a85510163a1a38d716dc
Pillys <pillys@163.com>
2016-07-19 00:14:48 +0800

    ++

--- js/my-app.js
+++ js/my-app.js
@@ -5,6 +5,7 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
+
 myApp.onPageInit('about', function (page) {
   $$('.create-page').on('click', function () {
     createContentPage();
@@ -15,6 +16,12 @@ myApp.onPageInit('index', function (page) {
   myApp.swiper('.swiper-container', {
     pagination:'.swiper-pagination',
     spaceBetween: 100
+  });
+  $$('form.searchbar').on('submit', function(e) {
+    var action = $$(this).attr('action'),
+      input = $$(this).find('input');
+    mainView.router.loadPage(action + '?' + input.attr('name') + '=' + encodeURIComponent(input.val()));
+    e.preventDefault();
   });
 });
 
