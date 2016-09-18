5655e269e67c8dc71604e6a8ddd516083398d182
Pillys <pillys@163.com>
2016-09-19 00:20:25 +0800

    ++

--- sunflowerseed/web/js/externalJs.js
+++ sunflowerseed/web/js/externalJs.js
@@ -211,7 +211,7 @@ ExternalJs.outboundScan = function(listId) {
           var exists = currentData.rows.find(function(v) {
             return v.pile.id == data.pile.id;
           });
-          if(seedId.val() !== '' && seedId.val() !== data.pile.seed.id) {
+          if(seedId.val() !== '' && seedId.val() !== data.pile.seed.id.toString()) {
             alert('出库信息与先期扫描不符('+ seedId.val() + '!='+ data.pile.seed.id + ')。');
             return false;
           }
